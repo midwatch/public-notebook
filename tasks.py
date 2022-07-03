7b7c1ce92bc4302b462a9e0c9c08c6a6336df1de
Justin Stout <midwatch@jstout.us>
2022-07-03 14:24:03 -0700

    implemented

--- tasks.py
+++ tasks.py
@@ -4,6 +4,8 @@ from invoke import Collection
 from invoke import task
 from invoke.exceptions import Failure
 
+from mw_dry_invoke import git
+
 GITHUB_USERNAME = "midwatch"
 GITHUB_SLUG = "public-notebook"
 CC_VERSION = "0.0.0"
@@ -25,48 +27,6 @@ def clean_build(ctx):
     Clean up files from package building
     """
     ctx.run("rm -fr build/")
-
-
-@task
-def scm_init(ctx):
-    """Init scm repo (if required).
-
-    Raises:
-        Failure: .gitignore does not exist
-
-    Returns:
-        None
-    """
-    if not Path('.gitignore').is_file():
-        raise Failure('.gitignore does not exist')
-
-    if not Path('.git').is_dir():
-        uri_remote = 'git@github.com:{}/{}.git'.format(GITHUB_USERNAME,
-                                                       GITHUB_SLUG
-                                                      )
-
-        ctx.run('git init')
-        ctx.run('git add .')
-        ctx.run('git commit -m "new package from midwatch/cc-py3-pkg ({})"'.format(CC_VERSION))
-        ctx.run('git branch -M main')
-        ctx.run('git remote add origin {}'.format(uri_remote))
-        ctx.run('git tag -a "v_0.0.0" -m "cookiecutter ref"')
-
-
-@task
-def scm_push(ctx):
-    """Push all branches and tags to origin."""
-
-    for branch in ('develop', 'main'):
-        ctx.run('git push origin {}'.format(branch))
-
-    ctx.run('git push --tags')
-
-
-@task
-def scm_status(ctx):
-    """Show status of remote branches."""
-    ctx.run('git for-each-ref --format="%(refname:short) %(upstream:track)" refs/heads')
 
 
 @task(pre=[clean_build])
@@ -95,30 +55,9 @@ def build(ctx):
 
 
 @task
-def new(ctx, template, path):
-    """
-    Add a new note from a template.
-    """
-    # template = template + '.rst.jinja'
-
-    opts = (
-        f'--template-dir {TEMPLATE_ROOT}',
-        f'--template-name {template}.rst.jinja',
-        path
-        )
-
-    # ctx.run(f'sphinx_notebook new note --template-dir {TEMPLATE_ROOT} --template-name {template} {path}')
-    ctx.run('sphinx_notebook new note {}'.format(' '.join(opts)))
-
-@task
-def init(ctx):
+def init_repo(ctx):
     """Initialize freshly cloned repo"""
-    scm_init(ctx)
-
-    ctx.run('git flow init -d')
-    ctx.run('git flow config set versiontagprefix v_')
-
-    scm_push(ctx)
+    git.init(ctx)
 
 
 @task(pre=[clean, build])
@@ -137,9 +76,5 @@ def release(ctx):
     ctx.run('gcloud app deploy')
 
 
-scm = Collection()
-scm.add_task(scm_push, name="push")
-scm.add_task(scm_status, name="status")
-
-ns = Collection(build, clean, init, new, release)
-ns.add_collection(scm, name="scm")
+ns = Collection(build, clean, init_repo, release)
+ns.add_collection(git.collection, name="scm")
